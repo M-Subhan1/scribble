58493c971f0674908cedae9e30ff4004be9b15a7
Subhan <msabbas.bscs20seecs@seecs.edu.pk>
2022-05-07 19:37:56 +0500

    Added Layout

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,64 @@
+<section>
+  <nav class="position-relative navbar py-2 px-4 px-md-10 navbar-expand-xl navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="wrexa-assets/logos/logo-wrexa.svg" alt="" width="auto">
+      </a>
+      <button class="d-xl-none navbar-burger btn btn-light p-0 d-flex justify-content-center align-items-center rounded-circle" style="width: 50px; height: 50px;" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <svg width="20" height="9" viewBox="0 0 20 9" fill="none" xmlns="http://www.w3.org/2000/svg"><line x1="0.75" y1="1.25" x2="19.25" y2="1.25" stroke="black" stroke-width="1.5" stroke-linecap="round"></line><line x1="0.75" y1="8.25" x2="13.25" y2="8.25" stroke="black" stroke-width="1.5" stroke-linecap="round"></line></svg>
+      </button>
+      <div class="position-absolute top-50 start-50 translate-middle mt-6 collapse navbar-collapse">
+        <ul class="navbar-nav">
+          <li class="nav-item dropdown position-relative">
+            <a class="nav-link pb-4 pe-10 dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Collection</a>
+            <div class="position-absolute bottom-0 start-0 w-100 bg-success-light" style="height: 3px;"></div>
+          </li>
+          <li class="nav-item"><a class="nav-link pb-4" href="#">Projects</a></li>
+          <li class="nav-item"><a class="nav-link pb-4" href="#">About</a></li>
+          <li class="nav-item dropdown"><a class="nav-link pb-4 dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">How to Start</a></li>
+          <li class="nav-item"><a class="nav-link pb-4" href="#">Design</a></li>
+        </ul>
+      </div>
+      <div class="collapse navbar-collapse">
+        <div class="ms-auto"><a class="btn btn-outline-light" href="#">Get Started</a></div>
+      </div>
+    </div>
+  </nav>
+  <div class="d-none navbar-menu position-relative" style="z-index: 99;">
+    <div class="navbar-backdrop position-fixed top-0 start-0 end-0 bottom-0 bg-dark opacity-75"></div>
+    <nav class="position-fixed top-0 start-0 bottom-0 w-75 mw-sm pt-6 bg-light">
+      <div class="d-flex flex-column px-6 pb-32 h-100 overflow-auto">
+        <div class="d-flex align-items-center mb-10">
+          <a class="me-auto h4 mb-0 text-decoration-none" href="#">
+            <img src="wrexa-assets/logos/logo-wrexa.svg" alt="" width="auto">
+          </a>
+          <button class="navbar-close btn-close" type="button" aria-label="Close"></button>
+        </div>
+        <div class="my-auto py-10">
+          <ul class="nav flex-column">
+            <li class="nav-item dropdown mb-6"><a class="nav-link dropdown-toggle p-0 text-dark" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Collection</a></li>
+            <li class="nav-item mb-6"><a class="nav-link p-0 text-dark" href="#">Projects</a></li>
+            <li class="nav-item mb-16"><a class="nav-link p-0 text-dark" href="#">About</a></li>
+            <li class="nav-item dropdown mb-6"><a class="nav-link dropdown-toggle p-0 text-dark" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">How to Start</a></li>
+            <li class="nav-item"><a class="nav-link p-0 text-dark" href="#">Design</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="position-absolute bottom-0 w-100">
+        <a class="btn py-5 px-4 d-flex align-items-center text-start mt-auto bg-white" href="#">
+          <div class="flex-shrink-0 position-relative me-4">
+            <img class="img-fluid rounded-circle" style="width: 48px; height: 48px;" src="wrexa-assets/images/avatar2.png" alt="">
+            <div class="position-absolute bottom-0 start-0 ms-n2 d-flex align-items-center justify-content-center rounded-circle bg-success text-white small" style="width: 23px; height: 23px;">1</div>
+          </div>
+          <div class="me-auto">
+            <h4 class="text-secondary small mb-0">Account</h4>
+            <span class="text-dark">matloay</span>
+          </div>
+          <svg width="12" height="7" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M10 2L6 6L2 2" stroke="black" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="square" stroke-linejoin="round"></path>
+          </svg>
+        </a>
+      </div>
+    </nav>
+  </div>
+</section>
