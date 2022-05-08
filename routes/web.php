50bf103a6227c01b1ca4f52193859af3f957b4e6
Subhan <msabbas.bscs20seecs@seecs.edu.pk>
2022-05-08 18:48:09 +0500

    Added Signup Alert

--- routes/web.php
+++ routes/web.php
@@ -3,22 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Response;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+/* |-------------------------------------------------------------------------- | Web Routes |-------------------------------------------------------------------------- | | Here is where you can register web routes for your application. These | routes are loaded by the RouteServiceProvider within a group which | contains the "web" middleware group. Now create something great! | */
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get("/login", function () {
+    if (array_key_exists('success', $_GET) && $_GET['success'] == 'true')
+        return view('login', ['alert' => (object)array('type' => 'success', 'message' => 'Account Created!')]);
+
     return view("login");
 });
 
@@ -26,6 +20,6 @@ Route::get("/register", function () {
     return view("register");
 });
 
-Route::post("/register", [AuthController::class, "register"]);
 
-Route::post("/login", [AuthController::class, "login"]);
+Route::post("/register", [AuthController::class , "register"]);
+Route::post("/login", [AuthController::class , "login"]);
