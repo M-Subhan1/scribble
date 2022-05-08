c51a1ff95c77af02f23ec1730314f7c21b0fa6d0
Subhan <msabbas.bscs20seecs@seecs.edu.pk>
2022-05-08 07:54:41 +0500

    Updated Env"

--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,28 @@
 // Burger menus
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
     // open
-    const burger = document.querySelectorAll('.navbar-burger');
-    const menu = document.querySelectorAll('.navbar-menu');
+    const burger = document.querySelectorAll(".navbar-burger");
+    const menu = document.querySelectorAll(".navbar-menu");
 
     if (burger.length && menu.length) {
         for (var i = 0; i < burger.length; i++) {
-            burger[i].addEventListener('click', function() {
+            burger[i].addEventListener("click", function () {
                 for (var j = 0; j < menu.length; j++) {
-                    menu[j].classList.toggle('d-none');
+                    menu[j].classList.toggle("d-none");
                 }
             });
         }
     }
 
     // close
-    const close = document.querySelectorAll('.navbar-close');
-    const backdrop = document.querySelectorAll('.navbar-backdrop');
+    const close = document.querySelectorAll(".navbar-close");
+    const backdrop = document.querySelectorAll(".navbar-backdrop");
 
     if (close.length) {
         for (var i = 0; i < close.length; i++) {
-            close[i].addEventListener('click', function() {
+            close[i].addEventListener("click", function () {
                 for (var j = 0; j < menu.length; j++) {
-                    menu[j].classList.toggle('d-none');
+                    menu[j].classList.toggle("d-none");
                 }
             });
         }
@@ -30,11 +30,24 @@ document.addEventListener('DOMContentLoaded', function() {
 
     if (backdrop.length) {
         for (var i = 0; i < backdrop.length; i++) {
-            backdrop[i].addEventListener('click', function() {
+            backdrop[i].addEventListener("click", function () {
                 for (var j = 0; j < menu.length; j++) {
-                    menu[j].classList.toggle('d-none');
+                    menu[j].classList.toggle("d-none");
                 }
             });
         }
     }
+
+    var toastElList = [].slice.call(document.querySelectorAll(".toast"));
+    var toastList = toastElList.map(function (toastEl) {
+        return new bootstrap.Toast(toastEl, {
+            animation: true,
+            autohide: true,
+            delay: true,
+        });
+    });
+
+    console.log(toastList);
+    // Toggle
+    toastList.forEach((toast) => toast.show());
 });
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -64,17 +64,26 @@
           <button class="btn btn-primary" type="submit">Create Account</button>
         </div>
       </form>
-      
-      @isset($alert)
-      <div class="toast align-items-center text-white alert-{{ $alert->type }} border-0 show" role="alert" aria-live="assertive" aria-atomic="true">
-          <div class="d-flex">
-            <div class="toast-body">
-              {{ $alert->message }}
-            </div>
-            <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+
+      <div class="m-4 position-fixed bottom-0 end-0 toast align-items-center text-white bg-success border-0" role="alert" aria-live="assertive" aria-atomic="true">
+        <div class="d-flex">
+          <div class="toast-body">
+            Hello, world! This is a toast message.
           </div>
+          <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
         </div>
-        @endisset
+      </div>
+
+      @isset($alert)
+      <div class="m-4 position-fixed bottom-0 end-0 toast align-items-center text-white bg-{{ $alert->type }} border-0 show" role="alert" aria-live="assertive" aria-atomic="true">
+        <div class="d-flex">
+          <div class="toast-body">
+            {{ $alert->message }}
+          </div>
+          <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+        </div>
+      </div>
+      @endisset
       </div>
     </div>
   </section>
