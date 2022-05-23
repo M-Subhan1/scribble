2eb6ad3e6f14cefa42c902ebf25c1ad35495de2d
Subhan <msabbas.bscs20seecs@seecs.edu.pk>
2022-05-23 10:08:40 +0500

    Fixed styling issues

--- resources/views/todo_display.blade.php
+++ resources/views/todo_display.blade.php
@@ -93,7 +93,7 @@
         <!-- Delete list modal -->
         <div class="modal fade" id="DeleteTodoModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
             aria-hidden="true">
-            <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+            <div class="modal-dialog modal-dialog-centered" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title" id="CreateModalLabel">Delete Your Todo</h5>
@@ -105,8 +105,8 @@
                         <h6>Are you sure you want to delete this todo?</h6>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-danger btn-sm" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary btn-sm todo_btn delete_list_btn">Delete</button>
+                        <button type="button" class="btn btn-dark btn-sm" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-danger btn-sm todo_btn delete_list_btn">Delete</button>
                     </div>
                 </div>
             </div>
